4cfa0d3a81c897dcecdcf4796161d0a3cce3ed7c
Nik Jiev <n.arabadjiev@gmail.com>
2017-11-13 04:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+companyName = 'AnotherCompanyName';
+
+let mostProfitableNeighborhood = 'Chelsea'
+mostProfitableNeighborhood = 'Upper West Side'
+
+let companyCeo = 'Susan Smith';
+companyCeo = 'Lauren Hart';
