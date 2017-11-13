7919c2c846bf48fc8acf5ddfcc2f060e58466b9b
Nik Jiev <n.arabadjiev@gmail.com>
2017-11-13 04:51:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const companyName = 'Scuber';
 companyName = 'AnotherCompanyName';
 
-let mostProfitableNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = Chelsea ;
 mostProfitableNeighborhood = 'Upper West Side';
 
 let companyCeo = 'Susan Smith';
